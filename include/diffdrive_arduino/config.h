56cc66b3cf1c7397bf9236932a7c5a764c3d03d9
alex_KubuntuVMWare20.04 <superiorsauserf191835@gmail.com>
2024-01-19 04:21:21 +0300

    Changed to connect by path

--- include/diffdrive_arduino/config.h
+++ include/diffdrive_arduino/config.h
@@ -9,7 +9,7 @@ struct Config
   std::string left_wheel_name = "left_wheel";
   std::string right_wheel_name = "right_wheel";
   float loop_rate = 30;
-  std::string device = "/dev/ttyUSB0";
+  std::string device = "/dev/serial/by-path/platform-fd500000.pcie-pci-0000:01:00.0-usb-0:1.4:1.0-port0"; // /dev/ttyUSB0
   int baud_rate = 57600;
   int timeout = 1000;
   int enc_counts_per_rev = 1560; // Changed for my encoders
